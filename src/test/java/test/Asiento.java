4bf921a676ca8649fdd734d21796372328346496
Andrés Aparicio <andres.apa.23@gmail.com>
2022-03-21 21:18:47 -0500

    Solción taller java int1

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,28 @@
+package test;
+public class Asiento{
+    String color;
+    int precio;
+    int registro;
+
+    public void cambiarColor(String color) {
+        switch (color) {
+            case "blanco":
+                this.color = color;
+                break;
+            case "verde":
+                this.color = color;
+                break;
+            case "rojo":
+                this.color = color;
+                break;
+            case "amarillo":
+                this.color = color;
+                break;  
+            case "negro":
+                this.color = color;
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,34 @@
+package test;
+public class Auto{
+    String modelo;
+    int precio;
+    Asiento[] asiento;
+    String marca;
+    Motor motor;
+    int registro;
+    static int cantidadCreados;
+
+    public int cantidadAsientos(){
+        count=0;
+        for(int i=0; i < this.asiento.length(); i++){
+            if(this.asiento[i]!=null){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public String verificarIntegridad(){
+        if(this.registro!=this.motor.registro){
+            return "Las piezas no son originales"
+        }
+        for(i=0; i<this.asiento.length; i++){
+            if(this.asiento[i]!=null){
+                if(this.asiento.registro!=this.registro){
+                    return "Las piezas no son originales"
+                }
+            }
+        }
+        return "Auto original"
+    }
+}
